416407ae8576f35ebedf69405c7db115f72cdafe
Rahul Thennarasu <rahulthennarasu07@gmail.com>
2025-11-13 16:50:28 -0800

    chore: add bootcamp-milestone-3b to gitignore

--- bootcamp-milestone-3b/src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      © 2025 Rahul Thennarasu's Personal Website | All Rights Reserved | Made with love ♥
-    </footer>
-  );
-}
